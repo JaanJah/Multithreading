ec8f4a9160709ef191230ef966b8e061fe61d10f
JaanJahimees <jaanjahilo@outlook.com>
2019-03-22 18:49:37 +0200

    Base project

---
+++ MultiThreading/MultiThreading/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MultiThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread th = Thread.CurrentThread;
+            th.Name = "MainThread";
+            Console.WriteLine(th.Name);
+            Console.ReadLine();
+        }
+    }
+}
