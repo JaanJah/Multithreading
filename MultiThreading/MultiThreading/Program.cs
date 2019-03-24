e69317f28ef0457c6323a2686e4f1f90cc4b31b6
JaanJahimees <jaanjahilo@outlook.com>
2019-03-24 21:00:17 +0200

    Basic example of multithreading.

--- MultiThreading/MultiThreading/Program.cs
+++ MultiThreading/MultiThreading/Program.cs
@@ -11,10 +11,40 @@ namespace MultiThreading
     {
         static void Main(string[] args)
         {
-            Thread th = Thread.CurrentThread;
-            th.Name = "MainThread";
-            Console.WriteLine(th.Name);
+            ThreadStart startThread1 = new ThreadStart(Thread1);
+            ThreadStart startThread2 = new ThreadStart(Thread2);
+            ThreadStart startThread3 = new ThreadStart(Thread3);
+
+            Thread thread1 = new Thread(startThread1);
+            Thread thread2 = new Thread(startThread2);
+            Thread thread3 = new Thread(startThread3);
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+
             Console.ReadLine();
+        }
+
+        public static void Thread1()
+        {
+            int interval = 2000;
+            Thread.Sleep(interval);
+            Console.WriteLine("Thread 1 finished.");
+        }
+
+        public static void Thread2()
+        {
+            int interval = 6000;
+            Thread.Sleep(interval);
+            Console.WriteLine("Thread 2 finished.");
+        }
+
+        public static void Thread3()
+        {
+            int interval = 4000;
+            Thread.Sleep(interval);
+            Console.WriteLine("Thread 3 finished.");
         }
     }
 }
